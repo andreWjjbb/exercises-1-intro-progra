558820656095fa0fdce0975be2b2c170073cafed
andreWjjbb <162144640+andreWjjbb@users.noreply.github.com>
2024-03-16 10:07:41 -0400

    3, 5, 6, 7

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,14 +6,24 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE
+  
 }
 
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
+  
 }
 
 void exercise_3(int a, int b) {
   // TODO: YOUR CODE HERE
+  if b=0;
+   cout<< "imposible";
+  else;
+   cout<< int a / b;
+  exercise_3(10, 2);
+  exercise_3(3, 5);
+  exercise_3(11, 0);
+
 }
 
 void exercise_4(double n, double a, double b, double x, double y) {
@@ -22,18 +32,62 @@ void exercise_4(double n, double a, double b, double x, double y) {
 
 void exercise_5(char character) {
   // TODO: YOUR CODE HERE
+  exercise_5(cin>>character);
+  if character >= 65 && <= 90;
+   cout<< "lower-case alphabet";
+  if-else character >= 97 && <= 122;
+   cout<< "upper-case alphabet";
+  else;
+   cout<<"not an alphabet"
 }
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
+  exercise_6(cin>>number);
+  switch(number);
+   case lunes;
+     if number == 1;
+     cout<<"Lunes";
+     break;
+   case martes;
+     if number == 2;
+     cout<<"Martes";
+     break;
+   case miercoles;
+     if number == 3;
+     cout<<"Miércoles";
+     break;
+   case jueves;
+     if number == 4;
+     cout<<"Jueves";
+     break;
+   case viernes;
+     if number == 5;
+     cout<<"Viernes";
+     break;
+   case sabado;
+     if number == 6;
+     cout<<"Sábado";
+     break;
+   case domingo;
+     if number == 7;
+     cout<<"Domingo";
+     break;        
 }
 
 void exercise_7(double r) {
   // TODO: YOUR CODE HERE
+  exercise_7(cin>> r);
+  if r < 0;
+   cout<<"Error: Radius cannot be negative.";
+  else;
+   cout<< double 4 * 3.1416 * (r * r);
+
 }
 
 void exercise_8(long int seconds) {
   // TODO: YOUR CODE HERE
+
 }
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
